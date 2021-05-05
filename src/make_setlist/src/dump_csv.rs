eaf683c2fed59d94e14df63ded084677b8df37fc
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-05 19:07:21 +0900

    feat: コマンドライン引数によって取得する年を変更

--- src/make_setlist/src/dump_csv.rs
+++ src/make_setlist/src/dump_csv.rs
@@ -1,6 +1,9 @@
 use csv::Writer;
 
-pub fn dump_csv(setlist: &Vec<Vec<String>>, filename: String) -> Result<(), Box<dyn std::error::Error>> {
+pub fn dump_csv(
+    setlist: &Vec<Vec<String>>,
+    filename: String,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut wtr = Writer::from_path(format!("out/{}.csv", filename))?;
     for row in setlist {
         wtr.write_record(row)?;
--- src/make_setlist/src/main.rs
+++ src/make_setlist/src/main.rs
@@ -1,9 +1,32 @@
+use clap::Clap;
 mod dump_csv;
+mod make_2018;
 mod make_2019;
 pub use crate::dump_csv::dump_csv;
+pub use crate::make_2018::make_setlist_2018;
 pub use crate::make_2019::make_setlist_2019;
 
+#[derive(Clap, Debug)]
+#[clap(
+    name = "make_setlist",
+    version = "0.1.0",
+    author = "xryuseix",
+    about = "Make Anisama setlist"
+)]
+struct Opts {
+    #[clap(name = "NUMBER")]
+    year: Option<i32>,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    make_setlist_2019()?;
+    let opts = Opts::parse();
+    match opts.year {
+        Some(year) => match year {
+            2018 => make_setlist_2018()?,
+            2019 => make_setlist_2019()?,
+            _ => return Ok(()),
+        },
+        None => return Ok(()),
+    };
     Ok(())
 }
