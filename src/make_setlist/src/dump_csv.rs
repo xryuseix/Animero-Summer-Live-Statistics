8b7e49d08937d1289e996b72f503b82dd861d771
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-05 18:16:45 +0900

    feat: dump csv

---
+++ src/make_setlist/src/dump_csv.rs
@@ -0,0 +1,10 @@
+use csv::Writer;
+
+pub fn dump_csv(setlist: &Vec<Vec<String>>, filename: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut wtr = Writer::from_path(format!("out/{}.csv", filename))?;
+    for row in setlist {
+        wtr.write_record(row)?;
+    }
+    wtr.flush()?;
+    Ok(())
+}
--- src/make_setlist/src/main.rs
+++ src/make_setlist/src/main.rs
@@ -1,4 +1,6 @@
+mod dump_csv;
 mod make_2019;
+pub use crate::dump_csv::dump_csv;
 pub use crate::make_2019::make_setlist_2019;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
