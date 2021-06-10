79eeef8fbc1ce5230724955e3b609e7060064d8a
xryuseix <ryusei143.shootingstar@gmail.com>
2021-06-10 10:03:46 +0900

    feat: 曲が存在するか判定する関数を追加

--- src/update_playlist/add_track.py
+++ src/update_playlist/add_track.py
@@ -33,6 +33,7 @@ def add(track_ids):
     print(response)
 
 
+# track_idの表記揺れを修正
 def shaping(track_id):
     is_url = re.fullmatch(r"^https://open.spotify.com/track/\w*(\?si=\w*)?$", track_id)
     if is_url != None:
@@ -46,6 +47,23 @@ def shaping(track_id):
         return None
 
 
+# 曲が存在するか確認する
+def is_exist(track_id):
+    with open(".OAuth_token") as f:
+        token = f.read()
+    end_point = f"https://api.spotify.com/v1/tracks/{track_id}"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+    track_info = requests.get(end_point, headers=headers).json()
+    if "error" in track_info:
+        return False
+    else:
+        return True
+
+
 def test():
     track_id = "2fJxU3u51B99Z3brrsOTTa"
 
@@ -55,6 +73,9 @@ def test():
     )
     assert shaping(f"https://open.spotify.com/track/{track_id}") == track_id
     assert shaping("https://") == None
+
+    assert is_exist(track_id) == True
+    assert is_exist(track_id + "skjhdfskl") == False
 
 
 if __name__ == "__main__":
